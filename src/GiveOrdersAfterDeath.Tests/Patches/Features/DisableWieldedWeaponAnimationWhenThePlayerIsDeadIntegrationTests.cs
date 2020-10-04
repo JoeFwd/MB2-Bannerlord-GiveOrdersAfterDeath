0fb7998683594dfc80b206d0534fe7f3218c8846
Joël Forward <jforward.dev@gmail.com>
2020-10-04 19:55:34 +0200

    add integration tests

---
+++ src/GiveOrdersAfterDeath.Tests/Patches/Features/DisableWieldedWeaponAnimationWhenThePlayerIsDeadIntegrationTests.cs
@@ -0,0 +1,45 @@
+﻿using GiveOrdersAfterDeath.Patches.Features;
+using GiveOrdersAfterDeath.Tests.Patches.Utilities;
+using HarmonyLib;
+using NUnit.Framework;
+using System.Linq;
+using System.Reflection;
+
+namespace GiveOrdersAfterDeath.Tests.Patches.Features
+{
+  public class DisableWieldedWeaponAnimationWhenThePlayerIsDeadIntegrationTests
+  {
+    private Harmony CreateDefaultHarmony()
+    {
+      return new Harmony(nameof (DisableWieldedWeaponAnimationWhenThePlayerIsDeadIntegrationTests));
+    }
+
+    private DisableWieldedWeaponAnimationWhenThePlayerIsDead CreateDefaultPatch()
+    {
+      return new DisableWieldedWeaponAnimationWhenThePlayerIsDead(CreateDefaultHarmony());
+    }
+
+    [Test]
+    public void OrderController_AfterSetOrder_Exists()
+    {
+      Assert.IsNotNull(CreateDefaultPatch().GetPatchMethodsInfo().ElementAt(0).PatchedMethod);
+    }
+
+    [Test]
+    public void TranspilerPatch_ShouldFindOnePatternOnly()
+    {
+      DisableWieldedWeaponAnimationWhenThePlayerIsDead defaultPatch = CreateDefaultPatch();
+      MethodInfo patchedMethod = defaultPatch.GetPatchMethodsInfo().ElementAt(0).PatchedMethod;
+      MethodInfo patchMethod = defaultPatch.GetPatchMethodsInfo().ElementAt(0).PatchMethod;
+      
+      CodeInstructionDifference codeInstructionDifference = TranspilerPatchHelper.CodeInstructionDifferencesBetweenOriginalAndTranspiledMethod(
+        defaultPatch,
+        patchedMethod,
+        patchMethod
+      );
+      
+      Assert.AreEqual(2, codeInstructionDifference.InsertedCodeInstructions.Count());
+      Assert.AreEqual(0, codeInstructionDifference.RemovedCodeInstructions.Count());
+    }
+  }
+}
---
+++ src/GiveOrdersAfterDeath.Tests/Patches/Utilities/CodeInstructionDifference.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GiveOrdersAfterDeath.Tests.Patches.Utilities
+{
+    public class CodeInstructionDifference
+    {
+        public CodeInstructionDifference(
+            IEnumerable<CodeInstruction> originalCodeInstructions,
+            IEnumerable<CodeInstruction> transpiledCodeInstructions)
+        {
+            this.InsertedCodeInstructions = transpiledCodeInstructions.ToList<CodeInstruction>().Except<CodeInstruction>(originalCodeInstructions);
+            this.RemovedCodeInstructions = originalCodeInstructions.ToList<CodeInstruction>().Except<CodeInstruction>(transpiledCodeInstructions);
+        }
+
+        public IEnumerable<CodeInstruction> RemovedCodeInstructions { get; }
+
+        public IEnumerable<CodeInstruction> InsertedCodeInstructions { get; }
+    }
+}
